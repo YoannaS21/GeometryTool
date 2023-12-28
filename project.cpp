ce159cca395b64e34c17629363f7552f3aa14a67
Yoanna <rositsa20@gmail.com>
2023-12-28 17:59:28 +0200

    Second tool

--- project.cpp
+++ project.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-using namespace std;
 #include <cmath>
+using namespace std;
 
 double inputPointX()
 {
@@ -46,18 +46,34 @@ bool isPointOnLine()
     return calculation < __DBL_EPSILON__;
 }
 
+void parallelLine()
+{
+    double x = inputPointX();
+    double y = inputPointY();
+    double slope = inputLineSlope();
+    double intercept = inputLineIntercept();
+
+    double parallelSlope = slope;
+    double parallelIntercept = (y - parallelSlope * x);
+
+    if (parallelIntercept != intercept)
+    {
+        cout << "The equasion of the parallel line is: y=" << parallelSlope << "x+" << parallelIntercept;
+    }
+}
+
 int main()
 {
 
     cout << "\n Choose a tool:\n"
             "1. Check whether a point lies on a line\n"
-            "2. \n"
+            "2. From a line g and a point p , derive an equation of a line that is parallel to g and passes through p\n"
             "3.Exit \n";
 
     while (true)
     {
         char choice;
-        cout << " Enter your choice: ";
+        cout << "Enter your choice: " << endl;
         cin >> choice;
 
         switch (choice)
@@ -69,9 +85,13 @@ int main()
             }
             else
             {
-                cout<<"The point does not lie on the line."<<endl;
+                cout << "The point does not lie on the line." << endl;
             }
 
+            break;
+
+        case '2':
+            parallelLine();
             break;
 
         case '3':
